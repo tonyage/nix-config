5adc44971aee531b21dcc7562d211a4d0d662769
Tony Do <tonyttdo@gmail.com>
2023-02-25 19:02:09 -0600

    docs: add README fill it out later

--- ssh.sh
+++ ssh.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 echo -e "Setting up new ssh keys."
+mkdir -p "$HOME/.ssh"
 pushd "$HOME/.ssh" > /dev/null || exit
   ssh-keygen -t ed25519 -f personal -q -N ""
   ssh-keygen -t ed25519 -f work -q -N ""
