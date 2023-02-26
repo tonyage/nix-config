779bbe039eb24aa0c37a9162aaacb5888de1a35a
Tony Do <tonyttdo@gmail.com>
2023-02-25 19:00:55 -0600

    adding script to setup ssh keys on new machines

---
+++ ssh.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo -e "Setting up new ssh keys."
+pushd "$HOME/.ssh" > /dev/null || exit
+  ssh-keygen -t ed25519 -f personal -q -N ""
+  ssh-keygen -t ed25519 -f work -q -N ""
+popd > /dev/null || exit
